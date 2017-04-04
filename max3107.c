9505d01af614195e68b23afd7ff274f2de892bbe
RonnieNigash <nigash@uw.edu>
2017-04-04 13:23:45 -0700

    Add macros for inserting module with params

--- max3107.c
+++ max3107.c
@@ -1,24 +1,39 @@
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/kernel.h> // KERN_ALERT
+#include <linux/init.h> // __init, __exit macros
 
+// Global variables
+static int loopback = 0;
+// End globals
 
-__init int max3107_init(void)
+MODULE_PARAM(loopback, "i"); // loopback is an integer type --> "i"
+MODULE_PARAM_DESC(loopback, "Set to 1 to enable loopback (connect rx and tx) on max3107");
+// usage: $ insmod max3107.ko loopback=1
+
+static void __init max3107_register_init(struct max3107_port *s)
+{
+
+}
+
+static int __init max3107_init(void)
 {
 	int major = register_chrdev(0, THIS_MODULE, &operations);
 
-	printk( KERN_INFO "%s: Registering device with %d\n", __FUNCTION__, major);
+	printk( KERN_ALERT "%s: Registering device with %d\n", __FUNCTION__, major);
 	if ( major < 0 ) {
-		printk( KERN_INFO "%s: Registering device failed with %d\n", __FUNCTION__, major);
+		printk( KERN_ALERT "%s: Registering device failed with %d\n", __FUNCTION__, major);
 		return major;
 	}
-	// @TODO: init the registers in max3107
+
+	// loop across uarts in group
+	// 	init registers
 	
 	return 0;
 }
 
 static void __exit max3107_exit(void)
 {
-	printk( KERN_INFO "%s: Registering device with %d\n", __FUNCTION__, major);
+	printk( KERN_ALERT "%s: Unregistering device with %d\n", __FUNCTION__, major);
 
 	// @TODO: unregister driver
 }
@@ -27,6 +42,6 @@ module_init(max3107_init);
 module_exit(max3107_exit);
 
 MODULE_DESCRIPTION("MAX3107 Driver");
-MODULE_AUTHOR("RonnieNigash");
+MODULE_AUTHOR("Ronnie Nigash");
 MODULE_ALIAS("max3107-i2c");
 MODULE_LICENSE("GPL");
