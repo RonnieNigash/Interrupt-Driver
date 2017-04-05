39acd7c6d7e70e5ff6e1c013e01cedc75609f3f2
RonnieNigash <nigash@uw.edu>
2017-04-05 14:38:35 -0700

    Update init method with error messages

--- max3107.c
+++ max3107.c
@@ -18,19 +18,23 @@ static void __init max3107_register_init(struct max3107_port *s)
 
 static int __init max3107_init(void)
 {
-	int major = register_chrdev(0, THIS_MODULE, &operations);
+	int registered;
 
-	printk( KERN_ALERT "%s: Registering device with %d\n", __FUNCTION__, major);
-	if ( major < 0 ) {
-		printk( KERN_ALERT "%s: Registering device failed with %d\n", __FUNCTION__, major);
-		return major;
+	registered = uart_register_driver(&max3107_uart);
+	if (registered) {
+		prink(KERN_ALERT "Registering MAX3107 Driver failed\n");
+		return registered;
 	}
 
-	// loop across uarts in group
-	// 	init registers
-	
-	return 0;
+	registered = i2c_add_driver(&max3107_i2c_uart_driver);
+	if (registered < 0) {
+		printk(KERN_ALERT "Failed to initalize max3107 uart i2c : %d\n", registered);
+		return registered;
+	}
+
+	return registered;
 }
+module_init(max3107_init);
 
 static void __exit max3107_exit(void)
 {
@@ -38,8 +42,6 @@ static void __exit max3107_exit(void)
 
 	// @TODO: unregister driver
 }
-
-module_init(max3107_init);
 module_exit(max3107_exit);
 
 MODULE_DESCRIPTION("MAX3107 Driver");
